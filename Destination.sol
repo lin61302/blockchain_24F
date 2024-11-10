de70b1f0ad96f2bfb78a48415b4c22b3997e1919
lin61302 <52012524+lin61302@users.noreply.github.com>
2024-11-09 19:32:30 -0500

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./BridgeToken.sol";
