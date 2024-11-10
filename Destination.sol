9a0671543761e7331094fd240e0c660cb0d536fd
lin61302 <52012524+lin61302@users.noreply.github.com>
2024-11-09 19:25:15 -0500

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -1,51 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./BridgeToken.sol";
 
-contract Destination is AccessControl, ReentrancyGuard {
-    // Define roles using keccak256 hash of role names
+contract Destination is AccessControl {
     bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
-
-    // Mapping from underlying token address to wrapped token address
     mapping(address => address) public underlying_tokens;
-
-    // Mapping from wrapped token address to underlying token address
     mapping(address => address) public wrapped_tokens;
-
-    // Array to keep track of all registered underlying tokens
     address[] public tokens;
 
-    // Events
     event Creation(address indexed underlying_token, address indexed wrapped_token);
-    event Wrap(
-        address indexed underlying_token,
-        address indexed wrapped_token,
-        address indexed to,
-        uint256 amount
-    );
-    event Unwrap(
-        address indexed underlying_token,
-        address indexed wrapped_token,
-        address indexed frm,
-        address to,
-        uint256 amount
-    );
-    event Debug(string message, address addr, uint256 num);
+    event Wrap(address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount);
+    event Unwrap(address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount);
 
-    /**
-     * @dev Constructor that sets up roles.
-     * @param admin The address that will have DEFAULT_ADMIN_ROLE, CREATOR_ROLE, and WARDEN_ROLE.
-     */
     constructor(address admin) {
-        require(admin != address(0), "Admin address cannot be zero");
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(CREATOR_ROLE, admin);
         _grantRole(WARDEN_ROLE, admin);
-        emit Debug("Constructor executed", admin, 0);
     }
 
     /**
@@ -69,16 +43,13 @@ contract Destination is AccessControl, ReentrancyGuard {
 
         // Deploy a new BridgeToken contract
         BridgeToken bridgeToken = new BridgeToken(_underlying_token, name, symbol, address(this));
-        emit Debug("BridgeToken deployed", address(bridgeToken), 0);
 
         // Store mappings
         underlying_tokens[_underlying_token] = address(bridgeToken);
         wrapped_tokens[address(bridgeToken)] = _underlying_token;
-        emit Debug("Mappings updated", _underlying_token, 0);
 
         // Add to tokens array
         tokens.push(_underlying_token);
-        emit Debug("Underlying token added to tokens array", _underlying_token, tokens.length);
 
         // Emit Creation event
         emit Creation(_underlying_token, address(bridgeToken));
@@ -97,7 +68,7 @@ contract Destination is AccessControl, ReentrancyGuard {
         address _underlying_token,
         address _recipient,
         uint256 _amount
-    ) public onlyRole(WARDEN_ROLE) nonReentrant {
+    ) public onlyRole(WARDEN_ROLE) {
         require(_underlying_token != address(0), "Invalid underlying token address");
         require(_recipient != address(0), "Invalid recipient address");
         require(_amount > 0, "Amount must be greater than zero");
@@ -108,15 +79,10 @@ contract Destination is AccessControl, ReentrancyGuard {
 
         // Get the wrapped token address
         address wrappedTokenAddress = underlying_tokens[_underlying_token];
-        require(wrappedTokenAddress != address(0), "Wrapped token address is zero");
-        emit Debug("Retrieved wrappedTokenAddress", wrappedTokenAddress, 0);
-
         BridgeToken wrappedToken = BridgeToken(wrappedTokenAddress);
-        emit Debug("BridgeToken instance created", wrappedTokenAddress, 0);
 
         // Mint BridgeTokens to the recipient
         wrappedToken.mint(_recipient, _amount);
-        emit Debug("Minted tokens", _recipient, _amount);
 
         // Emit Wrap event
         emit Wrap(_underlying_token, wrappedTokenAddress, _recipient, _amount);
@@ -133,7 +99,7 @@ contract Destination is AccessControl, ReentrancyGuard {
         address _wrapped_token,
         address _recipient,
         uint256 _amount
-    ) public nonReentrant {
+    ) public {
         require(_wrapped_token != address(0), "Invalid BridgeToken address");
         require(_recipient != address(0), "Invalid recipient address");
         require(_amount > 0, "Amount must be greater than zero");
@@ -146,36 +112,14 @@ contract Destination is AccessControl, ReentrancyGuard {
 
         // Get the underlying token address
         address underlyingTokenAddress = wrapped_tokens[_wrapped_token];
-        emit Debug("Underlying token address retrieved", underlyingTokenAddress, 0);
 
         // Instantiate the BridgeToken contract
         BridgeToken wrappedToken = BridgeToken(_wrapped_token);
-        emit Debug("BridgeToken instance created for burning", _wrapped_token, 0);
 
         // Burn the BridgeTokens from the caller
         wrappedToken.burn(_amount);
-        emit Debug("Burned tokens", msg.sender, _amount);
 
         // Emit Unwrap event
         emit Unwrap(underlyingTokenAddress, _wrapped_token, msg.sender, _recipient, _amount);
-
-        // Note: Actual transfer of underlying tokens back to the recipient on the source chain
-        // Typically handled off-chain or via cross-chain messaging protocols
-    }
-
-    /**
-     * @dev Returns the total number of registered underlying tokens.
-     * @return The count of underlying tokens registered.
-     */
-    function getRegisteredTokenCount() public view returns (uint256) {
-        return tokens.length;
-    }
-
-    /**
-     * @dev Returns all registered underlying tokens.
-     * @return An array of underlying token addresses.
-     */
-    function getAllUnderlyingTokens() public view returns (address[] memory) {
-        return tokens;
     }
 }
