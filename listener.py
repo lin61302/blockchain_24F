59bfa3fe2ef9458f4b3d41a28e7910902f55510c
lin61302 <52012524+lin61302@users.noreply.github.com>
2024-11-21 15:13:47 -0500

    Update listener.py

--- listener.py
+++ listener.py
@@ -58,7 +58,7 @@ def scanBlocks(chain,start_block,end_block,contract_address):
         event_filter = contract.events.Deposit.create_filter(fromBlock=start_block,toBlock=end_block,argument_filters=arg_filter)
         events = event_filter.get_all_entries()
         #print( f"Got {len(events)} entries for block {block_num}" )
-        // YOUR CODE HERE
+        ## YOUR CODE HERE
         # Process events and collect data
         for evt in events:
             data = {
@@ -77,7 +77,7 @@ def scanBlocks(chain,start_block,end_block,contract_address):
             event_filter = contract.events.Deposit.create_filter(fromBlock=block_num,toBlock=block_num,argument_filters=arg_filter)
             events = event_filter.get_all_entries()
             #print( f"Got {len(events)} entries for block {block_num}" )
-            // YOUR CODE HERE
+            ## YOUR CODE HERE
             # Process events and collect data
             for evt in events:
                 data = {
